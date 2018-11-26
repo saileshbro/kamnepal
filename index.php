1a215cbd1d63b17dd485859e70eac5c7c3f99810
Sailesh Dahal <saileshbro@gmail.com>
2018-11-26 19:58:05 +0545

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/all.css">
+
+    <title>Kam Nepal</title>
+</head>
+<body>
+    <!-- navbar here -->
+    <nav class="navbar">
+        <div class="navbar--left">
+            <i class="fa fa-graduation-cap brand-logo"></i>
+            <h1 class='brand-header'>Kam Nepal</h1>
+        </div>
+        <div class="navbar--right">
+            <div class="navbar-links">
+                <ul>
+                    <li><a href=""><i class="fa fa-search"></i></a></li>
+                    <li><a href=""><i class="far fa-bell"></i></a></li>
+                    <li><a href=""><i class="fa fa-cog"></i></a></li>
+                    <li><a href=""><img src="https://upload.wikimedia.org/wikipedia/commons/e/e0/Large_Scaled_Forest_Lizard.jpg" alt="a" style="width: 30px; height:30px;"></a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- end navbar -->    
+</body>
+</html>
