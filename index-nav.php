2cd2afe0221d3ae6293bc8a6d01efd0f2b930bf3
Sailesh Dahal <saileshbro@gmail.com>
2018-11-30 08:05:55 +0545

    popup created

---
+++ index-nav.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+
+    <title>Kam Nepal</title>
+</head>
+<body>
+    <!-- navbar here -->
+    <nav class="navbar">
+        <div class="navbar--left">
+            <img class='brand-logo' src="img/logo/kamnepal.svg" alt="" width='96px' href='./index.php'>
+            <!-- <a href='./index.php' class='brand-header links'>Kam Nepal</a> -->
+        </div>
+        <div class="navbar--right">
+            <div class="navbar-links">
+                <ul>
+                    <li><a href=""><img style="width: 30px; height:30px;" src="img/logo/search-solid.svg" alt=""></a></li>
+                    <li><a href=""><img style="width: 30px; height:30px;" src="img/logo/bell-regular.svg" alt=""></a></li>
+                    <li><a href=""><img style="width: 30px; height:30px;" src="img/logo/cog-solid.svg" alt=""></i></a></li>
+                    <li><a href=""><img src="https://upload.wikimedia.org/wikipedia/commons/e/e0/Large_Scaled_Forest_Lizard.jpg" alt="a" style="width: 30px; height:30px;"></a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- end navbar -->    
+</body>
+</html>
--- index.php
+++ index.php
@@ -4,29 +4,61 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="/css/main.css">
     <title>Kam Nepal</title>
 </head>
 <body>
-    <!-- navbar here -->
-    <nav class="navbar">
-        <div class="navbar--left">
-            <img class='brand-logo' src="img/logo/kamnepal.svg" alt="" width='96px' href='./index.php'>
-            <!-- <a href='./index.php' class='brand-header links'>Kam Nepal</a> -->
-        </div>
-        <div class="navbar--right">
+    <div class="landing-body">
+    <nav class="navbar-landing" id='#navbar-landing'>
+        <div class="navbar-left">
             <div class="navbar-links">
                 <ul>
-                    <li><a href=""><img style="width: 30px; height:30px;" src="img/logo/search-solid.svg" alt=""></a></li>
-                    <li><a href=""><img style="width: 30px; height:30px;" src="img/logo/bell-regular.svg" alt=""></a></li>
-                    <li><a href=""><img style="width: 30px; height:30px;" src="img/logo/cog-solid.svg" alt=""></i></a></li>
-                    <li><a href=""><img src="https://upload.wikimedia.org/wikipedia/commons/e/e0/Large_Scaled_Forest_Lizard.jpg" alt="a" style="width: 30px; height:30px;"></a></li>
+                    <li><a class='links' href="#">Find a job</a></li>
+                    <li><a class='links' href="#">Post a job</a></li>
+                </ul>
+            </div>    
+        </div>
+        <div class="navbar-center">
+            <img class='brand-logo' src="img/logo/kamnepal.svg" alt="" width='150rem' height='150rem' href='./index.php'>
+        </div>
+        <div class="navbar-right">
+            <div class="navbar-links">
+                <ul>
+                    <li><a href="#" class="links">Create a CV</a></li>
+                    <li><a href="#"  class="links" >Login</a>
+                    </li>
+                    <li><a href="#" id='register' onclick='myFunction()' class="links">Register</a>
+                    <div class="dropdown">
+                            <div class="dropdown-content">
+                                    <div>
+                                        <h3 class='dropdown-title'>Employer</h3>
+                                        <img src="img/logo/user-regular.svg" alt="">
+                                        <a href="" class="button-primary">Register</a>
+                                    </div>
+                                    <div>
+                                        <h3 class='dropdown-title'>Jobseeker</h3>
+                                        <img src="img/logo/building-regular.svg" alt="">
+                                        <a href="" class="button-primary">Register</a>
+                                    </div>
+                            </div>
+                        </div>
+                    </li>
                 </ul>
             </div>
         </div>
     </nav>
-    <!-- end navbar -->    
+    </div>
+    <br>
+    <div class="container" style='height:400px;'>
+        <div class="landing-jobs">
+            <h1 class='heading-secondary'>Hot jobs</h1>
+        </div>
+    </div>
+    <script>
+        function myFunction(){
+            var element = document.querySelector('#register');
+            console.log(document.querySelector('.dropdown').classList.toggle("show"));
+        }
+    </script>
 </body>
 </html>
