45067fdefafa87c1dfff052e1379b007f4b89567
Sailesh Dahal <saileshbro@gmail.com>
2018-12-25 08:24:09 +0545

    deleted buggy fontawesome files, dashboard nav to be modified

--- index-nav.php
+++ index-nav.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="css/fontawesome.css" rel="stylesheet">
     <link rel="stylesheet" href="css/main.css">
 
     <title>Kam Nepal</title>
@@ -13,12 +14,11 @@
     <nav class="navbar">
         <div class="navbar--left">
             <img class='brand-logo' src="img/logo/kamnepal.svg" alt="" width='96px' href='./index.php'>
-            <!-- <a href='./index.php' class='brand-header links'>Kam Nepal</a> -->
         </div>
         <div class="navbar--right">
             <div class="navbar-links">
                 <ul>
-                    <li><a href=""><img style="width: 30px; height:30px;" src="img/logo/search-solid.svg" alt=""></a></li>
+                    <li><a href=""><i class="fas fa-user"></i></a></li>
                     <li><a href=""><img style="width: 30px; height:30px;" src="img/logo/bell-regular.svg" alt=""></a></li>
                     <li><a href=""><img style="width: 30px; height:30px;" src="img/logo/cog-solid.svg" alt=""></i></a></li>
                     <li><a href=""><img src="https://upload.wikimedia.org/wikipedia/commons/e/e0/Large_Scaled_Forest_Lizard.jpg" alt="a" style="width: 30px; height:30px;"></a></li>
